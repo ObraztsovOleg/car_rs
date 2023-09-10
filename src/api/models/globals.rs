6c97e91183bac6698d2a5da51b3c4cba7e85b858
Carrow Shino <carrowsh@student.21-school.ru>
2023-09-10 13:39:22 +0300

    changes

--- src/api/models/globals.rs
+++ src/api/models/globals.rs
@@ -11,7 +11,7 @@ pub mod pwm {
     pub const PULSE: u64 = 1000;
     pub const SERVO_AVG_PULSE: u64 = 1500;
     pub const SERVO_MIN_PULSE: u64 = 1200;
-    pub const SERVO_MAX_PULSE: u64 = 2000;
+    pub const SERVO_MAX_PULSE: u64 = 1800;
     pub const SERVO_STEP: u64 = 50;
 
 }
