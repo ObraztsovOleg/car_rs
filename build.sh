9bab6570d6e694a809e50d155ff8fa8a7f7c4a81
Carrow Shino <carrowsh@student.21-school.ru>
2023-09-18 20:47:23 +0300

    changes

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cross build --target arm-unknown-linux-gnueabihf
-rm -rf ./server
-mv target/arm-unknown-linux-gnueabihf/debug/server ./
+scp target/arm-unknown-linux-gnueabihf/debug/server pi@192.169.31.5:~/rust_server/
